1e42aab4d3bf5e07f576ed2e2d267a1e1675e767
David <davidroth.br@gmail.com>
2021-02-11 11:11:09 -0500

    Spring - assignment

---
+++ src/main/java/com/montrealcollege/assignment/entities/Employee.java
@@ -0,0 +1,75 @@
+package com.montrealcollege.assignment.entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Double salary;
+    private String email;
+    @Autowired
+    private Department Department;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, Double salary, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public com.montrealcollege.assignment.entities.Department getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(com.montrealcollege.assignment.entities.Department department) {
+        Department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", email='" + email + '\'' +
+                ", Department=" + Department +
+                '}';
+    }
+}
